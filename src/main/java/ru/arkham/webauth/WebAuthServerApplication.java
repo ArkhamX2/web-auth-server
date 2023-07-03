d323f0c3a1a9269f72f507864d390acaf464484a
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-07-03 13:48:35 +0300

    update docs x2

--- src/main/java/ru/arkham/webauth/WebAuthServerApplication.java
+++ src/main/java/ru/arkham/webauth/WebAuthServerApplication.java
@@ -3,9 +3,16 @@ package ru.arkham.webauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Сервер авторизации.
+ */
 @SpringBootApplication
 public class WebAuthServerApplication {
 
+    /**
+     * Входная точка запуска.
+     * @param args аргументы.
+     */
     public static void main(String[] args) {
         SpringApplication.run(WebAuthServerApplication.class, args);
     }
